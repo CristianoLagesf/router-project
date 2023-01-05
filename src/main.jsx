d0193bac79ff64eb38ffbd44dbcfdd893727a39a
Cristiano Lages <cristianolagesf@hotmail.com>
2023-01-04 21:25:49 -0500

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,26 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import ErrorPage from "./Error-page";
 import "./index.css";
-import Root from "./routes/Root";
+import Contact, { loader as contactLoader } from "./routes/Contact";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+    ],
   },
 ]);
 
